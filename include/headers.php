40c07830628b76a32be51a857b955b70e29722f3
Ryan Wendel <ryan.wendel@gmail.com>
2018-04-22 12:56:19 -0600

    Create headers.php

---
+++ include/headers.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+header("Content-Security-Policy: default-src 'self'");
+header("Referrer-Policy: origin");
+header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
+header("X-Content-Type-Options: nosniff");
+header("X-Frame-Options: DENY");
+header("X-XSS-Protection: 1; mode=block");
+*/
+
+?>
