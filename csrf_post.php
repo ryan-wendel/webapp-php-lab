5624cae08d8db5e6dd476480fad31745e8438192
Ryan Wendel <ryan.wendel@gmail.com>
2018-04-28 16:16:19 -0600

    Update csrf_post.php

--- csrf_post.php
+++ csrf_post.php
@@ -5,7 +5,6 @@
 <?php
 include "include/headers.php";
 
-
 // simulating a database thru flat files because lazy
 $profile    = './data/profile.csv';
 $token_file = './data/token.csv';
@@ -15,29 +14,30 @@ $new_token = uniqid();
 file_put_contents($token_file, $new_token);
 
 if (isset($_POST['input'])) {
-	$input = urldecode($_POST['input']);
+    $input = urldecode($_POST['input']);
 }
 
 if (isset($_POST['csrf_token'])) {
-	$post_token = $_POST['csrf_token'];
+    $post_token = $_POST['csrf_token'];
 
-	echo "<p>Old token = " . $old_token . "</p>\n";
-	echo "<p>Post token = " . $post_token . "</p>\n";
+    echo "<p>Old token = " . $old_token . "</p>\n";
+    echo "<p>Post token = " . $post_token . "</p>\n";
 
-	if(strcmp($post_token, $old_token) == 0) {
-		echo "<p style='color: red;'><strong>Success</strong>: You've changed the profile value.</p>\n";
-		file_put_contents($profile, $input);
+    if(strcmp($post_token, $old_token) == 0) {
+        echo "<p style='color: red;'><strong>Success</strong>: You've changed the profile value.</p>\n";
+        file_put_contents($profile, $input);
     }
 }
 
 // grab and output profile value
 $profile_value = file_get_contents($profile);
 echo "<p>Profile value = " . $profile_value . "</p>\n";
+
 ?>
 <form id="form1" action="csrf_post.php" method="post">
-	<input type="text" name="input">
-	<input type="hidden" name="csrf_token" value="<?php echo $new_token; ?>">
-	<input type="submit">
+    <input type="text" name="input">
+    <input type="hidden" name="csrf_token" value="<?php echo $new_token; ?>">
+    <input type="submit">
 </form>
 </body>
-</html>
+</html>	
