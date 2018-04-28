5a7e5266335c6579bb1431f53ed86db120bbf509
Ryan Wendel <ryan.wendel@gmail.com>
2018-04-28 16:15:12 -0600

    Update lfi_get.php

--- lfi_get.php
+++ lfi_get.php
@@ -11,23 +11,22 @@ setcookie("test_cookie", $value, null, '/', null, null, false);
 setcookie("xsrf_token", uniqid(), null, '/', null, null, $httponly);
 
 if (isset($_GET['include'])) {
-	$include = urldecode($_GET['include']);
+    $include = urldecode($_GET['include']);
 
-	echo "<p>GET['include'] = $include</p>\n";
-	
-	if(file_exists($include)) {
-		include "$include";
-		
-		echo "<br /><br />\n";
-	
-		highlight_file($include);
-	} else {
-		echo "<p style='color: red;'>Unable to open file: $include</p>";
-	}
+    echo "<p>GET['include'] = $include</p>\n";
+
+    if(file_exists($include)) {
+        include "$include";
+
+        echo "<br /><br />\n";
+
+        highlight_file($include);
+    } else {
+        echo "<p style='color: red;'>Unable to open file: $include</p>";
+    }
 } else {
-	echo "<p>GET['include'] is empty</p>\n";
+    echo "<p>GET['include'] is empty</p>\n";
 }
 ?>
 </body>
 </html>
-
