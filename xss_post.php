9c8578a0dfd96b3465a0cd436b544e3b14940a17
Ryan Wendel <ryan.wendel@gmail.com>
2018-04-28 17:16:27 -0600

    Update xss_post.php

--- xss_post.php
+++ xss_post.php
@@ -1,26 +1,27 @@
 <html>
 <body>
-<h3 id="title">XSS Vulnerable Form</h3>
+<h2 id="title">XSS Vulnerable Form</h2>
 <?php
 include "include/headers.php";
 
 $value = 'foo-bar';
 $httponly = true;
+$xsrf_token = uniqid();
 
 setcookie("test_cookie", $value, null, '/', null, null, false);
-setcookie("xsrf_token", uniqid(), null, '/', null, null, $httponly);
+setcookie("xsrf_token", $xsrf_token, null, '/', null, null, $httponly);
 
 if (isset($_POST['input'])) {
-    $input = urldecode($_POST['input']);
-
-    echo "<p id='input'>POST['input'] = $input</p>\n";
+	$input = urldecode($_POST['input']);
+	
+	echo "<p id='input'>POST['input'] = $input</p>\n";
 } else {
-    echo "<p>POST['input'] is empty</p>\n";
+	echo "<p>POST['input'] is empty</p>\n";
 }
 ?>
-<form action="xss_post.php" method="post">
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
     <input type="text" name="input">
-    <input type="hidden" name="xsrf_token" value="<?php echo uniqid(); ?>">
+    <input type="hidden" name="xsrf_token" value="<?php echo $xsrf_token; ?>">
     <input type="submit">
 </form>
 </body>
