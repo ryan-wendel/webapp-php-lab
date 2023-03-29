f4d5c10c678782c719e57997aebab6ae58426963
Ryan Wendel <ryan.wendel@gmail.com>
2023-03-29 14:38:09 -0600

    Update csrf_post.php

--- csrf_post.php
+++ csrf_post.php
@@ -1,9 +1,13 @@
 <html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/default.css">
+</head>
 <body>
 <h2 id="title">Vulnerable Profile Change Page</h2>
 <p>Input a new profile value.</p>
 <?php
 include "include/headers.php";
+
 
 // simulating a database thru flat files because lazy
 $profile    = './data/profile.csv';
@@ -14,7 +18,7 @@ $new_token = uniqid();
 file_put_contents($token_file, $new_token);
 
 if (isset($_POST['input'])) {
-    $input = urldecode($_POST['input']);
+        $input = urldecode($_POST['input']);
 
     if (isset($_POST['csrf_token'])) {
         $post_token = $_POST['csrf_token'];
@@ -23,8 +27,8 @@ if (isset($_POST['input'])) {
         echo "<p>Post token = " . $post_token . "</p>\n";
 
         if(strcmp($post_token, $old_token) == 0) {
-            echo "<p style='color: red;'><strong>Success</strong>: You've changed the profile value.</p>\n";
-            file_put_contents($profile, $input);
+                echo "<p style='color: red;'><strong>Success</strong>: You've changed the profile value.</p>\n";
+                file_put_contents($profile, $input);
         }
     }
 }
@@ -32,12 +36,11 @@ if (isset($_POST['input'])) {
 // grab and output profile value
 $profile_value = file_get_contents($profile);
 echo "<p>Profile value = " . $profile_value . "</p>\n";
-
 ?>
 <form id="form1" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    <input type="text" name="input">
-    <input type="hidden" name="csrf_token" value="<?php echo $new_token; ?>">
-    <input type="submit">
+        <input type="text" name="input">
+        <input type="hidden" name="csrf_token" value="<?php echo $new_token; ?>">
+        <input type="submit">
 </form>
 </body>
-</html>	
+</html>
