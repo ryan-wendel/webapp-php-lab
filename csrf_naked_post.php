efb5ee4849f35262f700b0a567868abd1836a9db
Ryan Wendel <ryan.wendel@gmail.com>
2023-03-29 14:38:24 -0600

    Update csrf_naked_post.php

--- csrf_naked_post.php
+++ csrf_naked_post.php
@@ -1,4 +1,7 @@
 <html>
+<head>
+        <link rel="stylesheet" type="text/css" href="css/default.css">
+</head>
 <body>
 <h2 id="title">Vulnerable Profile Change Page</h2>
 <p>No CSRF token is required.</p>
@@ -6,13 +9,19 @@
 <?php
 include "include/headers.php";
 
+$value = 'foo-bar';
+$httponly = false;
+
+setcookie("test_cookie", $value, null, '/', null, null, $httponly);
+setcookie("xsrf_token", uniqid(), null, '/', null, null, $httponly);
+
 // simulating a database thru flat files because lazy
 $profile    = './data/profile.csv';
 
 if (isset($_POST['input'])) {
-    $input = urldecode($_POST['input']);
+        $input = urldecode($_POST['input']);
 
-    echo "<p style='color: red;'><strong>Success</strong>: You've changed the profile value.</p>\n";
+    echo "<p class='red'><strong>Success</strong>: You've changed the profile value.</p>\n";
     file_put_contents($profile, $input);
 }
 
@@ -21,8 +30,8 @@ $profile_value = file_get_contents($profile);
 echo "<p>Profile value = " . $profile_value . "</p>\n";
 ?>
 <form id="form1" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    <input type="text" name="input">
-    <input type="submit">
+        <input type="text" name="input">
+        <input type="submit">
 </form>
 </body>
 </html>
